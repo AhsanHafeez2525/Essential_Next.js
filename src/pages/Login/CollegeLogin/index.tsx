7af51d273cb4eb22265151c9ae0f23f2da1385b3
Ahsan <127826249+AhsanHafeezJoynGroup@users.noreply.github.com>
2024-05-04 16:42:37 +0500

    nested-routing

---
+++ src/pages/Login/CollegeLogin/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CollegeLogin = () => {
+	return (
+		<div>
+			<h1>Welcome to college login</h1>
+		</div>
+	);
+};
+
+export default CollegeLogin;
---
+++ src/pages/Login/StudentLogin/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentLogin = () => {
+	return (
+		<div>
+			<h1>Welcome to student login screen</h1>
+		</div>
+	);
+};
+
+export default StudentLogin;
