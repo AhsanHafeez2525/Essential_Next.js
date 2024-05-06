4aa646be2ae4f2c522c4dfe06691308f6ddfd304
Ahsan <127826249+AhsanHafeezJoynGroup@users.noreply.github.com>
2024-05-06 22:27:44 +0500

    conditional-layout

--- src/app/Login/layout.tsx
+++ src/app/Login/layout.tsx
@@ -1,18 +1,42 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 export default function Layout({ children }) {
+	const pathName = usePathname();
+	console.log(pathName);
 	return (
+		// <div>
+		// 	{pathName !== '/Login/CollegeLogin' ? (
+		// 		<ul>
+		// 			<li>
+		// 				<Link href='/Login'>Login Main</Link>
+		// 			</li>
+		// 			<li>
+		// 				<Link href='/Login/StudentLogin'>Student Login</Link>
+		// 			</li>
+		// 			<li>
+		// 				<Link href='/Login/CollegeLogin'>Teacher Login</Link>
+		// 			</li>
+		// 		</ul>
+		// 	) : null}
+		// 	{children}
+		// </div>
 		<div>
-			<ul>
-				<li>
-					<Link href='/Login'>Login Main</Link>
-				</li>
-				<li>
-					<Link href='/Login/StudentLogin'>Student Login</Link>
-				</li>
-				<li>
-					<Link href='/Login/CollegeLogin'>Teacher Login</Link>
-				</li>
-			</ul>
+			{pathName !== '/Login/CollegeLogin' ? (
+				<ul>
+					<li>
+						<Link href='/Login'>Login Main</Link>
+					</li>
+					<li>
+						<Link href='/Login/StudentLogin'>Student Login</Link>
+					</li>
+					<li>
+						<Link href='/Login/CollegeLogin'>Teacher Login</Link>
+					</li>
+				</ul>
+			) : (
+				<Link href={'/Login'}>Go to main login page</Link>
+			)}
 			{children}
 		</div>
 	);
