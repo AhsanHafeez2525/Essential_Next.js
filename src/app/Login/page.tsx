227536e15dff244f3fa3cd8df536306a8e8b1d4a
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2024-05-20 22:50:01 +0500

    add-module-css

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { IoSearchOutline } from "react-icons/io5";
+import Custom from "../custom.module.css";
 
 const Login = () => {
-	return (
-		<div>
-			<h1>Welcome to login page</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Welcome to login page</h1>
+      <h2 className={Custom.main}>you reach on</h2>
+      <div>
+        <IoSearchOutline size={20} />
+        <input type="text" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
