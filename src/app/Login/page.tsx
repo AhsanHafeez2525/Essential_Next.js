cb5c6d07ab23496ece39d2998f03c3a54aa627fb
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2024-05-22 00:41:14 +0500

    image-optimize

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { IoSearchOutline } from "react-icons/io5";
 import Custom from "../custom.module.css";
+import Image from "next/image";
+import VercelLogo from "../../../public/vercel.svg";
 
 const Login = () => {
   return (
@@ -10,6 +12,13 @@ const Login = () => {
       <div>
         <IoSearchOutline size={20} />
         <input type="text" />
+        <Image src={VercelLogo} width={100} height={100} alt="vercellogo" />
+        <Image
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz14OUetvXQ7HhiX7I4iPztp-nzIq2DArMnmvxN1L52Q&s"
+          width={400}
+          height={400}
+          alt="coding"
+        />
       </div>
     </div>
   );
